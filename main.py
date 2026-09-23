4dbb33c716034045e0800eeda93574be20a3e2ff
cpakeo <113116116+cpakeo@users.noreply.github.com>
2026-09-23 18:48:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ def get_weather(region, config):
     # 当前温度
     temp = response["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
     # 风向
-    wind_dir = response["now"]["windDir"]
+    wind = response["now"]["windDir"]
     # 【新增】风力等级
     windScale = response["now"]["windScale"]
     # 【新增】降雨量（过去1小时降水量，单位mm）
